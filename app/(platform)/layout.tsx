893abd84165204ebb5f9be8ee66df08bc9be3389
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2024-10-22 20:29:47 +0530

    Feat: Initial Commit

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,15 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+const PlatformLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default PlatformLayout;

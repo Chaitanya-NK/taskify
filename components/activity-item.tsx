acfaee70bdbae94b45f4ddb5b4b07d9a4af955a0
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2024-10-26 18:13:43 +0530

    Fix: Alignment issue in activity logs

--- components/activity-item.tsx
+++ components/activity-item.tsx
@@ -16,7 +16,7 @@ export const ActivityItem = ({
             <Avatar className="h-8 w-8">
                 <AvatarImage src={data.userImage} />
             </Avatar>
-            <div className="flex flex-col; space-y-0.5">
+            <div className="flex flex-col space-y-0.5">
                 <p className="text-sm text-muted-foreground">
                     <span className="font-semibold lowercase text-neutral-700">
                         {data.userName}
